7933dfebe6520bac13b870a0a84c5261f4c9d56f
srividyan1 <33527870+srividyan1@users.noreply.github.com>
2021-06-04 17:49:52 -0400

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -114,9 +114,11 @@ def index():
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
-            logging.info('Dogs')
+            logger.setLevel(logging.INFO)
+            logger.info('Dogs')
             vote2 = r.get(button2).decode('utf-8')
-            logging.info('Cats')
+            logger.setLevel(logging.INFO)
+            logger.info('Cats')
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
